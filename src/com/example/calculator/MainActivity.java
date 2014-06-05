5a7cacca6bc81d19d2b1b7b998905bd9c08a0ee4
jenny <yk494@cornell.edu>
2014-06-05 16:50:34 +0900

    first commit

---
+++ src/com/example/calculator/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+		Log.d("debug", "hybygby");
+	}
+	
+	
+}
